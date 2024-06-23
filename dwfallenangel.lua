af246202c5c12f2825dafe5cd9025e4a32522779
gdnoob1324 <gdnoob1324@gmail.com>
2024-06-23 18:39:52 +0900

    u

--- dwfallenangel.lua
+++ dwfallenangel.lua
@@ -1,3 +1,4 @@
+repeat task.wait() until game:IsLoaded()
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/mercury-lib-edit/master/src.lua"))()
 
 local GUI = Library:Create{
@@ -12,16 +13,13 @@ local Main = GUI:tab{
     Icon = "rbxassetid://8569322835" -- rbxassetid://2174510075 home icon
 }
 
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local VirtualInputManager = game:GetService("VirtualInputManager")
+local Players = cloneref(game:GetService("Players"))
+local ReplicatedStorage = cloneref(game:GetService("ReplicatedStorage"))
+local VirtualInputManager = cloneref(game:GetService("VirtualInputManager"))
 
 local lp = Players.LocalPlayer
 local Systems = ReplicatedStorage:WaitForChild("Systems")
 
-local Race = lp.PlayerGui.Score.Frame.Race
-local Timer
-local Laps
 local material
 
 local Driveworld = {}
@@ -33,6 +31,17 @@ for i,v in pairs(getconnections(Players.LocalPlayer.Idled)) do
         v["Disconnect"](v)
     end
 end
+
+local oldnamecall
+oldnamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local method = getnamecallmethod() 
+    if not checkcaller() and method == "InvokeServer" and self.Name == "QuitJob" then
+        if (Driveworld["autodeliveryfood"] or Driveworld["autodelivery"]) then
+            return wait(9e9)
+        end
+    end
+    return oldnamecall(self, ...)
+end))
 
 local function getchar()
     return lp.Character or lp.CharacterAdded:Wait()
@@ -71,9 +80,9 @@ end
 
 Main:Toggle({
     Name = "Auto Delivery Truck",
-    StartingState = false,
+	StartingState = false,
     Description = "Use Full-E or Casper for more money(work in USA map only) wait for 40 sec",
-    Callback = function(state)
+	Callback = function(state)
         Driveworld["autodelivery"] = state
     end
 })
@@ -90,9 +99,9 @@ Main:Dropdown{
 
 Main:Toggle({
     Name = "Auto Delivery Material",
-    StartingState = false,
+	StartingState = false,
     Description = "wait 25 sec",
-    Callback = function(state)
+	Callback = function(state)
         Driveworld["autodeliverymaterial"] = state
         if state == false then
             ReplicatedStorage:WaitForChild("Systems"):WaitForChild("Contracts"):WaitForChild("EndContract"):InvokeServer()
@@ -103,9 +112,9 @@ Main:Toggle({
 
 Main:Toggle({
     Name = "Auto Delivery Food",
-    StartingState = false,
+	StartingState = false,
     Description = "wait for 20 sec",
-    Callback = function(state)
+	Callback = function(state)
         Driveworld["autodeliveryfood"] = state
     end
 })
@@ -135,7 +144,7 @@ task.spawn(function()
                     CompletionRegion = workspace:FindFirstChild("CompletionRegion")
                 end
             until CompletionRegion or Driveworld["autodeliveryfood"] == false
-            for i = 1, 18 do
+            for i = 1, 20 do
                 if not Driveworld["autodeliveryfood"] or not getvehicle() or not getchar() or isvehicle() == false or job.Visible == false then
                     break
                 end
@@ -150,7 +159,7 @@ task.spawn(function()
                     v.Main.Velocity += v.Main.CFrame.LookVector * Vector3.new(100,0,100) - Vector3.new(0,0,0)
                 end
             end
-            task.wait(.1)
+            task.wait(.5)
             Systems:WaitForChild("Jobs"):WaitForChild("CompleteJob"):InvokeServer()
             task.wait(.5)
             if lp.PlayerGui.JobComplete.Enabled == true then
@@ -202,7 +211,7 @@ task.spawn(function()
                     ReplicatedStorage:WaitForChild("Systems"):WaitForChild("Jobs"):WaitForChild("StartJob"):InvokeServer(workspace:WaitForChild("Jobs"):WaitForChild("Trucking"),workspace:WaitForChild("Jobs"):WaitForChild("Trucking"):WaitForChild("StartPoints"):WaitForChild("Logs"))
                 end
             until jobDistance and tonumber(jobDistance) >= 2.1 or Driveworld["autodelivery"] == false
-            for i = 1, 40 do
+            for i = 1, 35 do
                 if not Driveworld["autodelivery"] or not getvehicle() or not getchar() or isvehicle() == false or job.Visible == false then
                     break
                 end
@@ -214,12 +223,19 @@ task.spawn(function()
             VirtualInputManager:SendKeyEvent(true, "W", false, game)
             for _,v in pairs(workspace.Cars:GetChildren()) do
                 if tostring(v.Owner.Value) == game.Players.LocalPlayer.Name then
-                    v.Main.Velocity += v.Main.CFrame.LookVector * Vector3.new(5,0,5)
+                    v.Main.Velocity += v.Main.CFrame.LookVector * Vector3.new(10,0,10)
                 end
             end
-            task.wait(.1)
+            task.wait(.5)
             Systems:WaitForChild("Jobs"):WaitForChild("CompleteJob"):InvokeServer()
             VirtualInputManager:SendKeyEvent(false, "W", false, game)
+            task.wait(.5)
+            if lp.PlayerGui.JobComplete.Enabled == true then
+                Systems:WaitForChild("Jobs"):WaitForChild("CashBankedEarnings"):FireServer()
+                for i,v in next, getconnections(lp.PlayerGui.JobComplete.Window.Content.Buttons.CloseButton.MouseButton1Click) do
+                    v:Fire()
+                end
+            end
             print("Completed Job")
         end
     end
@@ -297,13 +313,7 @@ task.spawn(function()
             end
             if completepos then
                 getvehicle():SetPrimaryPartCFrame(completepos)
-                for _,v in pairs(workspace.Cars:GetChildren()) do
-                    if tostring(v.Owner.Value) == game.Players.LocalPlayer.Name then
-                        v.Main.Velocity += v.Main.CFrame.LookVector * Vector3.new(100,0,100) - Vector3.new(0,0,0)
-                    end
-                end
-
-                task.wait(.1)
+                task.wait(1)
                 ReplicatedStorage:WaitForChild("Systems"):WaitForChild("Contracts"):WaitForChild("DropoffCargo"):InvokeServer()
             end
         end
@@ -315,11 +325,6 @@ GUI:Credit{
     Description = "Made the script",
     V3rm = "https://v3rmillion.net/member.php?action=profile&uid=2270329",
     Discord = "https://discord.gg/b9QX5rnkT5"
-}
-
-GUI:Credit{
-    Name = "gdnoob1324",
-    Description = "Edit the script",
 }
 
 GUI:set_status("Status | Active")
